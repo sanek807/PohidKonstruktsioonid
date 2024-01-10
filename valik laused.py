0afad2942808d90eb78ee598348b3a6f02ddcd0d
opilane <opilane@HP-CZC2349HTF>
2024-01-10 15:55:25 +0200

    Valik Laused

---
+++ valik laused.py
@@ -0,0 +1,26 @@
+from random import *
+
+
+a=randint(-100,100)
+if a%2==0:
+    print("Juhuslik arv on",a)
+    print(a,"-paaris arv")
+
+if a>0:
+    print(a,"suurem kui 0")
+else:
+    print(a,"väiksem kui 0 või võrdne 0-ga")
+
+#<0,>100 ei soobi,0-59-"2",60-75-"3",76-90-"4",91-100-"5"
+if a<0 or a>100:
+    print("Viga tulemustega!")
+elif a>=0 and a<60:
+    print("Hinne 2")
+elif a>=60 and a<76:
+    print("Hinne 3")
+elif a>=76 and a<91:
+    print("Hinne 4")
+else:
+    print("Hinne 5")
+
+
