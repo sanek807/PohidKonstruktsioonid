b36ec16f180055ed1a92a2a60f9214f4d5f01461
opilane <opilane@HP-CZC2349HTF>
2024-01-17 16:22:44 +0200

    Indeks massq tela

--- Indeks massq tela.py
+++ Indeks massq tela.py
@@ -1,27 +1,54 @@
-print("Tere! Olen sinu uus sõber - Python!")
-nimi = input("Sisesta oma nimi: ")
-print(nimi + ", oi kui ilus nimi!")
-vastus = input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
-if vastus==1:
-    pikkus=int(input("Pikkus: "))
-    mass=float(input("Mass: "))
-    indeks=int(input("Indeks: " mass / (0.01 * pikkus) ** 2))
-    print(f"{name}! Sinu keha indeks on: {indeks:.1f}")
-    if indeks_massi_tela < 16:
-    vastus = "Tervisele ohtlik alakaal"
-elif 16 <= indeks_massi_tela < 19:
-    vastus = "Alakaal"
-elif 19 <= indeks_massi_tela < 25:
-    vastus = "Normaalkaal"
-elif 25 <= indeks_massi_tela < 30:
-    vastus = "Ülekaal"
-elif 30 <= indeks_massi_tela < 35:
-    vastus = "Rasvumine"
-elif 35 <= indeks_massi_tela < 40:
-    vastus = "Tugev rasvumine"
-else:
-    vastus = "Tervisele ohtlik rasvumine"
+##1
+#for k in range(1.16):
+#    n=float(input("Sisesta arv nr. "+str(k)))
+#    if int(n)==float(n):
+#        mitu+=1
+#    if k==5: break
 
+
+
+
+
+   
+    
+
+
+
+            
+
+             
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print("Tere! Olen sinu uus sõber - Python!")
+#nimi = input("Sisesta oma nimi: ")
+#print(nimi + ", oi kui ilus nimi!")
+#vastus = input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
+#if vastus==1:
+#    pikkus=int(input("Pikkus: "))
+#    mass=float(input("Mass: "))
+#    indeks=int(input("Indeks: " mass / (0.01 * pikkus) ** 2))
+
+    
+    
 
 
 
