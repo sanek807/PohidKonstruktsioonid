5b2100451e0866a0ab5d4c137007760dd9abf15b
opilane <opilane@HP-CZC2349HTF>
2024-01-17 09:56:25 +0200

    Indeks massq tela

---
+++ Indeks massq tela.py
@@ -0,0 +1,6 @@
+print("Tere! Olen sinu uus sõber - Python!")
+nimi = input("Sisesta oma nimi: ")
+print(nimi + ", oi kui ilus nimi!")
+vastus = input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
+
+
