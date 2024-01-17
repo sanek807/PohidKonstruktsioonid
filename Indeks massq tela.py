b0676d9f08f4f5881416d6c5920af266ce8a8ddc
opilane <opilane@HP-CZC2349HTF>
2024-01-17 10:01:07 +0200

    Indeks massq tela

--- Indeks massq tela.py
+++ Indeks massq tela.py
@@ -2,5 +2,10 @@ print("Tere! Olen sinu uus sõber - Python!")
 nimi = input("Sisesta oma nimi: ")
 print(nimi + ", oi kui ilus nimi!")
 vastus = input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => ")
+if vastus==1:
+    pikkus=int(input("Pikkus: "))
+    mass=input(input("Mass: "))
+    indeks=int(input("Indeks: " mass / (0.01 * pikkus) ** 2))
+
 
 
